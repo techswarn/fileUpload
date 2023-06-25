23a37bdb43cf81bb21eb792ce2d12b31992a00bb
techswarn <tech.swarns@gmail.com>
2023-06-25 17:08:07 +0530

    code update

--- app.js
+++ app.js
@@ -1,68 +1,76 @@
-
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3 } from "@aws-sdk/client-s3";
-import 'dotenv/config'
+import "dotenv/config";
 
-require('dotenv').config('.env')
+require("dotenv").config(".env");
 
 const uploadInput = document.getElementById("uploadInput");
-const upload = document.querySelector(".form-submit")
-const alert = document.getElementById("alert-success")
+const upload = document.querySelector(".form-submit");
+const alert = document.getElementById("alert-success");
 
 const s3Client = new S3({
-    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
-    endpoint: "https://nyc3.digitaloceanspaces.com",
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.ACCESS_ID,
-      secretAccessKey: process.env.ACCESS_SECRET_KEY
-    }
+  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+  endpoint: "https://nyc3.digitaloceanspaces.com",
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.ACCESS_ID,
+    secretAccessKey: process.env.ACCESS_SECRET_KEY,
+  },
 });
 
 const createBucketParams = (file, content) => {
-    return {
-        Bucket: "backend",
-        Key: file,
-        Body: content
+  return {
+    Bucket: "backend",
+    Key: file,
+    Body: content,
+  };
+};
+
+const uploadFunc = async (bucketParams) => {
+  try {
+    if (uploadInput.files[0].name === null) {
+      console.log("Please choose file");
     }
-}
+    const data = await s3Client.send(new PutObjectCommand(bucketParams));
+    console.log(
+      "Successfully uploaded object: " +
+        bucketParams.Bucket +
+        "/" +
+        bucketParams.Key
+    );
+    alert.classList.add("alert-success");
+    alert.innerHTML =
+      "Successfully uploaded object: " +
+      bucketParams.Bucket +
+      "/" +
+      bucketParams.Key;
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+    alert.innerHTML = "Error, please check console for more details";
+  }
+  console.log("uploading");
+};
 
-const uploadFunc = async(bucketParams) => {
-    try {
-        const data = await s3Client.send(new PutObjectCommand(bucketParams));
-        console.log(
-          "Successfully uploaded object: " +
-            bucketParams.Bucket +
-            "/" +
-            bucketParams.Key
-        );
-        alert.classList.add("alert-success")
-        alert.innerHTML = "Successfully uploaded object: " + bucketParams.Bucket + "/" + bucketParams.Key
-        return data;
-      } catch (err) {
-        console.log("Error", err);
-        alert.innerHTML = "Error, please check console for more details"
-      }
-    console.log('uploading')
-}
-
-let files = []
+let files = [];
 uploadInput.addEventListener(
   "change",
   () => {
-    alert.classList.remove("alert-success")
-    console.log(uploadInput.files[0])
-    const bucketParams = createBucketParams(uploadInput.files[0].name, uploadInput.files[0] )
-    console.log(bucketParams)
-    upload.addEventListener("click", async () => {        
-        try {
-          const data = await uploadFunc(bucketParams)
-          console.log(data)
-        } catch(err) {
-          console.log(err)
-        }
-
-    })
+    alert.classList.remove("alert-success");
+    console.log(uploadInput.files[0]);
+    const bucketParams = createBucketParams(
+      uploadInput.files[0].name,
+      uploadInput.files[0]
+    );
+    console.log(bucketParams);
+    upload.addEventListener("click", async () => {
+      try {
+        const data = await uploadFunc(bucketParams);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    });
   },
   false
 );
